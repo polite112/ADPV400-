0f17f77034e58eec436193ca170fcddf378b145c
Sithole P <polite123.ps@gmail.com>
2021-05-14 14:58:53 +0200

    first commit

---
+++ multiply.java
@@ -0,0 +1,20 @@
+public class Add {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        //declaration
+        int num1;
+        int num2;
+        System.out.println("Enter num1");
+        num1 = scan.nextInt();
+        
+        System.out.println("Enter num2");
+        num2 = scan.nextInt();
+        
+        int sum = num1 * num2;
+        System.out.println("Mul is: " + sum);
+        
+                
+                
+    }
+   
